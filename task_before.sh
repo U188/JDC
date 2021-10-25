bdb7026834536510a86ed6fd8f9a300af0a5a82e
U188 <54225629+U188@users.noreply.github.com>
2021-10-25 14:27:12 +0800

    Update task_before.sh

--- task_before.sh
+++ task_before.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Build 20210810-003
+# Build 20211009-001-test
 
 name_js=(
   jd_fruit
@@ -79,7 +79,7 @@ var_name=(
   TokenJxnc                           ## 17、京喜Token(京喜财富岛提现用)
 )
 
-## 临时屏蔽某账号运行活动脚本
+## 临时屏蔽某账号运行活动脚本(账号序号匹配)
 TempBlock_JD_COOKIE(){
     source $file_env
     local TempBlockCookieInterval="$(echo $TempBlockCookie | perl -pe "{s|~|-|; s|_|-|}" | sed 's/\(\d\+\)-\(\d\+\)/{\1..\2}/g')"
@@ -99,6 +99,109 @@ TempBlock_JD_COOKIE(){
     temp_user_sum=${#array[*]}
 }
 
+## 临时屏蔽某账号运行活动脚本(pt_pin匹配)
+TempBlock_JD_PT_PIN(){
+    [[ -z $JD_COOKIE ]] && source $file_env
+    local TempBlockPinArray=($TempBlockPin)
+    local envs=$(eval echo "\$JD_COOKIE")
+    local array=($(echo $envs | sed 's/&/ /g'))
+    local i m n t pt_pin_temp pt_pin_temp_block
+    for i in "${!array[@]}"; do
+        pt_pin_temp=$(echo ${array[i]} | perl -pe "{s|.*pt_pin=([^; ]+)(?=;?).*|\1|; s|%|\\\x|g}")
+        [[ $pt_pin_temp == *\\x* ]] && pt_pin[i]=$(printf $pt_pin_temp) || pt_pin[i]=$pt_pin_temp
+        for n in "${!TempBlockPinArray[@]}"; do
+            pt_pin_temp_block=$(echo ${TempBlockPinArray[n]} | perl -pe "{s|%|\\\x|g}")
+            [[ $pt_pin_temp_block == *\\x* ]] && pt_pin_block[n]=$(printf $pt_pin_temp_block) || pt_pin_block[n]=$pt_pin_temp_block
+            [[ "${pt_pin[i]}" =~ "${pt_pin_block[n]}" ]] && unset array[i]
+        done
+    done
+    jdCookie=$(echo ${array[*]} | sed 's/\ /\&/g')
+    [[ ! -z $jdCookie ]] && export JD_COOKIE="$jdCookie"
+    temp_user_sum=${#array[*]}
+}
+
+## 随机账号运行活动
+Random_JD_COOKIE(){
+    [[ -z $JD_COOKIE ]] && source $file_env
+    local envs=$(eval echo "\$JD_COOKIE")
+    local array=($(echo $envs | sed 's/&/ /g'))
+    local user_sum=${#array[*]}
+    local combined_all
+    if [[ $RandomMode = "1" ]]; then
+        [[ ! $ran_num ]] && ran_num=$user_sum
+        if [ $(echo $ran_num|grep '[0-9]') ]; then
+            [[ $ran_num -gt $user_sum || $ran_num -lt 2 ]] && ran_num=$user_sum
+            ran_sub="$(seq $user_sum | sort -R | head -$ran_num)"
+            for i in $ran_sub; do
+                tmp="${array[i]}"
+                combined_all="$combined_all&$tmp"
+            done
+            jdCookie=$(echo $combined_all | sed 's/^&//g')
+            [[ ! -z $jdCookie ]] && export JD_COOKIE="$jdCookie"
+        fi
+    fi
+}
+
+## 组队任务
+combine_team(){
+    p=$1
+    q=$2
+    export jd_zdjr_activityId=$3
+    export jd_zdjr_activityUrl=$4
+}
+
+team_task(){
+    [[ -z $JD_COOKIE ]] && source $file_env
+    local envs=$(eval echo "\$JD_COOKIE")
+    local array=($(echo $envs | sed 's/&/ /g'))
+    local user_sum=${#array[*]}
+    local i j k x y p q
+    local scr=$scr_name
+    local teamer_array=($teamer_num)
+    local team_array=($team_num)
+    if [[ -f /ql/scripts/$scr ]]; then
+        for ((i=0; i<${#teamer_array[*]}; i++)); do
+            combine_team ${teamer_array[i]} ${team_array[i]} ${activityId[i]} ${activityUrl[i]}
+            [[ $q -ge $(($user_sum/p)) ]] && q=$(($user_sum/p))
+	    [[ q -lt 1 ]] && q=1
+            for ((m = 0; m < $user_sum; m++)); do
+                j=$((m + 1))
+                x=$((m/q))
+                y=$(((p - 1)*m + 1))
+                COOKIES_HEAD="${array[x]}"
+                COOKIES=""
+                if [[ $j -le $q ]]; then
+                    for ((n = 1; n < $p; n++)); do
+                        COOKIES="$COOKIES&${array[y]}"
+                        let y++
+                    done
+                elif [[ $j -eq $((q + 1)) ]]; then
+                    for ((n = 1; n < $((p-1)); n++)); do
+                        COOKIES_HEAD="${array[x]}&${array[0]}"
+                        COOKIES="$COOKIES&${array[y]}"
+                        let y++
+                    done
+                elif [[ $j -gt $((q + 1)) ]]; then
+                    [[ $((y+1)) -le $user_sum ]] && y=$(((p - 1)*m)) || break
+                    for ((n = $m; n < $((m + p -1)); n++)); do
+                        COOKIES="$COOKIES&${array[y]}"
+                        let y++
+                        [[ $y = $x ]] && y=$((y+1))
+                        [[ $((y+1)) -gt $user_sum ]] && break
+                    done
+                fi
+                result=$(echo -e "$COOKIES_HEAD$COOKIES")
+                if [[ $result ]]; then
+                    export JD_COOKIE=$result
+                    node /ql/scripts/$scr
+                fi
+#               echo $JD_COOKIE
+            done
+        done
+        exit
+    fi
+}
+
 ## 组合互助码格式化为全局变量的函数
 combine_sub() {
     source $file_env
@@ -110,7 +213,7 @@ combine_sub() {
     local envs=$(eval echo "\$JD_COOKIE")
     local array=($(echo $envs | sed 's/&/ /g'))
     local user_sum=${#array[*]}
-    local a b i j t sum
+    local a b i j t sum combined_all
     for ((i=1; i <= $user_sum; i++)); do
         local tmp1=$what_combine$i
         local tmp2=${!tmp1}
@@ -142,11 +245,33 @@ combine_all() {
     done
 }
 
-for ((i = 0; i < ${#env_name[*]}; i++)); do
-    export ${env_name[i]}=""
-done
+## 正常依次运行时，组合互助码格式化为全局变量
+combine_only() {
+    for ((i = 0; i < ${#env_name[*]}; i++)); do
+        case $1 in
+            *${name_js[i]}*.js | *${name_js[i]}*.ts)
+	            if [[ -f $dir_log/.ShareCode/${name_config[i]}.log ]]; then
+                    . $dir_log/.ShareCode/${name_config[i]}.log
+                    result=$(combine_sub ${var_name[i]})
+                    if [[ $result ]]; then
+                        export ${env_name[i]}=$result
+                    fi
+                fi
+                ;;
+           *)
+                export ${env_name[i]}=""
+                ;;
+        esac
+    done
+}
 
-TempBlock_JD_COOKIE
+TempBlock_JD_COOKIE && TempBlock_JD_PT_PIN && Random_JD_COOKIE
+
+if [ $scr_name ]; then
+    team_task
+else
+	combine_only "$1"
+fi
 
 #if [[ $(ls $dir_code) ]]; then
 #    latest_log=$(ls -r $dir_code | head -1)
@@ -154,19 +279,3 @@ TempBlock_JD_COOKIE
 #    combine_all
 #fi
 
-for ((i = 0; i < ${#env_name[*]}; i++)); do
-    case $1 in
-        *${name_js[i]}.js | *${name_js[i]}.ts)
-	    if [[ -f $dir_log/.ShareCode/${name_config[i]}.log ]]; then
-                . $dir_log/.ShareCode/${name_config[i]}.log
-                result=$(combine_sub ${var_name[i]})
-                if [[ $result ]]; then
-                    export ${env_name[i]}=$result
-                fi
-            fi
-            ;;
-        *)
-            export ${env_name[i]}=""
-            ;;
-    esac
-done
